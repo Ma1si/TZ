1f7eb3cb24b3b0e53a6926a880b2912326e3a291
Ma1si <islamhick@gmail.com>
2026-04-05 21:33:28 +0300

    Update db.py

--- TZpython/src/db.py
+++ TZpython/src/db.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from models.models import Base
 
-engine = create_async_engine("postgresql+asyncpg://postgres:fubkz13love@localhost:5432/TZpython", echo=True, )
+engine = create_async_engine("postgresql+asyncpg://postgres:пароль@localhost:5432/имя БД", echo=True, )
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def create_table():
